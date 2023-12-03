bcc9cbdef18684d777acb764b34a81f2d721650b
Jason Long <jlong5795@gmail.com>
2023-12-02 18:48:31 -0600

    adding stub for day 1 part 2

--- day1.js
+++ day1.js
@@ -1007,3 +1007,22 @@ calibrations.forEach((calibration) => {
   const newNumber = numbers[0] + numbers[numbers.length - 1];
   return (total += parseInt(newNumber));
 });
+
+// Part 2
+calibrations.forEach((calibration) => {
+  calibration
+    .replace(/\bone\b/g, "1")
+    .replace(/\btwo\b/g, "2")
+    .replace(/\bthree\b/g, "3")
+    .replace(/\bfour\b/g, "4")
+    .replace(/\bfive\b/g, "5")
+    .replace(/\bsix\b/g, "6")
+    .replace(/\bseven\b/g, "7")
+    .replace(/\beight\b/g, "8")
+    .replace(/\bnine\b/g, "9");
+  // .replace(/\bzero\b/g, '0')
+  const numbers = calibration.replace(/[^0-9]/g, "");
+  const newNumber = numbers[0] + numbers[numbers.length - 1];
+  return (total += parseInt(newNumber));
+});
+console.log(total);
